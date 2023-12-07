62279cf03deaf136af243ad9cbc6cd3f75804e5a
Jeeho Ahn <io@jcde.xyz>
2023-12-07 23:47:58 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -42,8 +42,8 @@ export default function Home() {
           width={500}
           height={333}
           data-scroll
-          data-scroll-delay="0"
-          data-scroll-speed="-0.2"
+          data-scroll-delay="0.5"
+          data-scroll-speed="-0.3"
           src="https://images.unsplash.com/photo-1525286335722-c30c6b5df541?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         />
       </div>
